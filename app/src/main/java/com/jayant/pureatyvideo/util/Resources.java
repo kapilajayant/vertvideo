8a34ed8aade3dd7bbe79a0c546b686a8618caff6
kapilajayant <kapilajayant@gmail.com>
2020-04-22 20:46:56 +0530

    Fixed limit issue

--- app/src/main/java/com/jayant/pureatyvideo/util/Resources.java
+++ app/src/main/java/com/jayant/pureatyvideo/util/Resources.java
@@ -6,19 +6,19 @@ import com.jayant.pureatyvideo.models.MediaObject;
 public class Resources {
 
     public static final MediaObject[] MEDIA_OBJECTS = {
-            
-            new MediaObject("Go Pro Vertical Video",
-                    "https://firebasestorage.googleapis.com/v0/b/blackcoffer-task.appspot.com/o/GoPro%20Vertical%20Video%20(WATCH%20ON%20MOBILE).mp4?alt=media&token=cfd17611-ac60-433b-9f4f-48a2b86cd09c",
+
+            new MediaObject("Leaves",
+                    "https://firebasestorage.googleapis.com/v0/b/kewl-a649c.appspot.com/o/Fall%20Colors%20-%20Vertical%20Video.mp4?alt=media&token=ec714988-6f1b-4f1e-8e83-e87dc6ca714c",
+                    "https://s3.ca-central-1.amazonaws.com/codingwithmitch/media/VideoPlayerRecyclerView/REST+API%2C+Retrofit2%2C+MVVM+Course+SUMMARY.png",
+                    "Description for media object #1"),
+            new MediaObject("Waterfall",
+                    "https://firebasestorage.googleapis.com/v0/b/kewl-a649c.appspot.com/o/ART%2035%20VERTICAL%20VIDEO%20WALLPAPERS.mp4?alt=media&token=4bd7f231-c3d8-4b39-a4f1-1ab0caadb064",
                     "https://s3.ca-central-1.amazonaws.com/codingwithmitch/media/VideoPlayerRecyclerView/REST+API%2C+Retrofit2%2C+MVVM+Course+SUMMARY.png",
                     "Description for media object #2"),
-            new MediaObject("Go Pro Vertical Video",
-                    "https://firebasestorage.googleapis.com/v0/b/blackcoffer-task.appspot.com/o/GoPro%20Vertical%20Video%20(WATCH%20ON%20MOBILE).mp4?alt=media&token=cfd17611-ac60-433b-9f4f-48a2b86cd09c",
+            new MediaObject("Angry Birds",
+                    "https://firebasestorage.googleapis.com/v0/b/farmer-data-1298.appspot.com/o/AngryBirds%20GTST%20Slingshot%201080x1920%2015s%20NU.mp4?alt=media&token=c106ab71-6d42-4120-a068-75809c8b1c4a",
                     "https://s3.ca-central-1.amazonaws.com/codingwithmitch/media/VideoPlayerRecyclerView/REST+API%2C+Retrofit2%2C+MVVM+Course+SUMMARY.png",
-                    "Description for media object #2"),
-            new MediaObject("Go Pro Vertical Video",
-                    "https://firebasestorage.googleapis.com/v0/b/blackcoffer-task.appspot.com/o/GoPro%20Vertical%20Video%20(WATCH%20ON%20MOBILE).mp4?alt=media&token=cfd17611-ac60-433b-9f4f-48a2b86cd09c",
-                    "https://s3.ca-central-1.amazonaws.com/codingwithmitch/media/VideoPlayerRecyclerView/REST+API%2C+Retrofit2%2C+MVVM+Course+SUMMARY.png",
-                    "Description for media object #2"),
+                    "Description for media object #3"),
             new MediaObject("Spider in web",
                     "https://firebasestorage.googleapis.com/v0/b/pureatyvideo.appspot.com/o/Spider%20in%20Web%20MVI%201009%20001%201080x1920%20h264.mp4?alt=media&token=ad5023da-5f71-41c4-86c8-ccffc57b256b",
                     "https://s3.ca-central-1.amazonaws.com/codingwithmitch/media/VideoPlayerRecyclerView/Swiping+Views+with+a+ViewPager.png",
